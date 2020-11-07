730404f8014679fe0974c09ffc515edb148f55a7
Austin Merrick <austin.merrick@unlv.edu>
2020-11-06 17:10:00 -0800

    Removing extra output

--- assignment4/main.py
+++ assignment4/main.py
@@ -19,12 +19,6 @@ classifier = KNeighborsClassifier( 3 )
 clf = make_pipeline(StandardScaler(), classifier)
 fitted = clf.fit(X, Y)
 
-print("coef:")
-#print(classifier.coef_)
-
-print("intercept:")
-#print(classifier.intercept_)
-
 print("score on training:")
 print( fitted.score(X, Y) )
 
@@ -37,55 +31,3 @@ Y = data['Y']
 print( fitted.score(X, Y) )
 
 print("iterations:")
-#print( classifier.n_iter_)
-
-
-
-
-
-
-
-# print("===========")
-# print("Ordinary least squares")
-# print("===========")
-
-# # Ordinary least squares
-# reg = LinearRegression().fit(X, y)
-
-# # test fitness on test data
-# print("test score:")
-# test = pd.read_csv('test.csv')
-# print( reg.score(test[['X2','X3','X4']], test['Y']) )
-# print("train score:")
-# print( reg.score(X, y) )
-
-# # coef result
-# print("coefs:")
-# print( reg.coef_ )
-# # intercept result
-# print("intercept:")
-# print( reg.intercept_ )
-
-# print("===========")
-# print("Linear regression with gradient descent")
-# print("===========")
-
-# # Ordinary least squares
-# sgd = SGDRegressor(max_iter=1000000, tol=1e-3, alpha=.0001)
-# reg = make_pipeline(StandardScaler(), sgd)
-# fitted = reg.fit(X,y)
-
-# print("iterations:")
-# print( sgd.n_iter_)
-# print("test score:")
-# test = pd.read_csv('test.csv')
-# print( fitted.score( test[['X2','X3','X4']], test['Y']) )
-# print("train score:")
-# print( fitted.score(X, y) )
-
-# # coef result
-# print("coefs:")
-# print(sgd.coef_)
-# # intercept result
-# print("intercept:")
-# print(sgd.intercept_)
